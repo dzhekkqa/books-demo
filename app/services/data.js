13bab4bef425a81440c9658c3a2a2b686eca8e9c
Решетников Евгений Константинович <ekreshetnikov@edu.hse.ru>
2021-03-14 21:11:26 +0500

    Получение данных

--- app/services/data.js
+++ app/services/data.js
@@ -1,12 +1,27 @@
 import Service from '@ember/service';
 import ENV from 'books-demo/config/environment';
+import { A } from '@ember/array';
 
 export default Service.extend({
-  getSpeakers() {
-    return fetch(`${ENV.backendURL}/speakers`).then((response) => response.json());
+  init() {
+    this._super(...arguments);
+    this.set('speakers', A());
   },
 
-  getSpeaker(id) {
-    return fetch(`${ENV.backendURL}/speakers/${id}`).then((response) => response.json());
+  async getSpeakers() {
+    let response = await fetch(`${ENV.backendURL}/speakers`);
+    let speakers = await response.json();
+    this.get('speakers').clear();
+    this.get('speakers').pushObjects(speakers);
+    return this.get('speakers');
+  },
+
+  getSpeaker(id) {    
+    return this.get('speakers').find(speaker => speaker.id === parseInt(id));
+  },
+
+  deleteSpeaker(speaker) {
+    this.get('speakers').removeObject(speaker);
+    return fetch(`${ENV.backendURL}/speakers/${speaker.id}`, { method: 'DELETE' });
   }
 });
